2f920bfa868589e800a794d6e443b19579c1232e
Martin Disch <martindisch@gmail.com>
2018-12-22 10:07:57 +0100

    Add additional timeouts
    Even though we're waiting until elements are shown on the page, it
    doesn't seem to work sometimes.

--- connectboxcontrol/util/control.py
+++ connectboxcontrol/util/control.py
@@ -16,7 +16,10 @@ def control(password, action=0):
         The password to login with
     action : int, optional
         Whether to turn WiFi on (1) or off (0)
+
     """
+    # Additional timeout for Selenium to be safe
+    timeout = 2
     # Prepare Xvfb display
     print("Preparing display")
     display = Display(visible=0, size=(800, 600))
@@ -30,17 +33,22 @@ def control(password, action=0):
     # Fill out login form and submit
     print("Logging in")
     login_pw = wait.until(EC.element_to_be_clickable((By.ID, 'loginPassword')))
+    time.sleep(timeout)
     login_pw.send_keys(password)
     continue_button = wait.until(EC.element_to_be_clickable((By.ID, 'c_42')))
+    time.sleep(timeout)
     continue_button.click()
 
     # Find the WiFi settings and load the page
     print("Opening WiFi settings")
     adv_settings = wait.until(EC.element_to_be_clickable((By.ID, 'c_mu05')))
+    time.sleep(timeout)
     adv_settings.click()
     wifi_settings = wait.until(EC.element_to_be_clickable((By.ID, 'c_mu06')))
+    time.sleep(timeout)
     wifi_settings.click()
     wifi_page = wait.until(EC.element_to_be_clickable((By.ID, 'c_mu07')))
+    time.sleep(timeout)
     wifi_page.click()
     # Give the page time to load completely (checkbox won't work otherwise)
     time.sleep(5)
@@ -49,14 +57,17 @@ def control(password, action=0):
     if action == 1:
         print("Turning WiFi on")
         on = wait.until(EC.element_to_be_clickable((By.ID, 'iwlanRadio2G1')))
+        time.sleep(timeout)
         on.click()
     elif action == 0:
         print("Turning WiFi off")
         off = wait.until(EC.element_to_be_clickable((By.ID, 'iwlanRadio2G2')))
+        time.sleep(timeout)
         off.click()
 
     # Apply settings
     apply = wait.until(EC.element_to_be_clickable((By.ID, 'c_02')))
+    time.sleep(timeout)
     apply.click()
     # Wait until settings are applied (checkboxes clickable again)
     wait.until(EC.element_to_be_clickable((By.ID, 'iwlanRadio2G1')))
