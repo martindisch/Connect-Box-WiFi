8c4e2d4491b86e208bbc0692e9fae4712050df93
Martin Disch <martindisch@gmail.com>
2020-12-27 16:37:41 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="connectboxcontrol",
-    version="1.0.0",
+    version="1.1.0",
     description="Turn the WiFi of your Connect Box router on/off",
     packages=['connectboxcontrol'],
     include_package_data=True,
